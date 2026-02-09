c0652c7f096874afed6ad27173b36759e133a240
elikromi <68767698+elikromi@users.noreply.github.com>
2026-02-09 13:57:37 +0700

    Add files via upload

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,8 +3,8 @@ import { createClient } from '@supabase/supabase-js';
 
 // Ganti nilai di bawah ini dengan URL dan Anon Key dari Dashboard Supabase Anda
 // Project Settings > API
-const supabaseUrl = 'https://your-project-url.supabase.co';
-const supabaseAnonKey = 'your-anon-key';
+const supabaseUrl = 'https://vletczmrxsyvwvsjxrnx.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsZXRjem1yeHN5dnd2c2p4cm54Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA1OTk4MjgsImV4cCI6MjA4NjE3NTgyOH0.kpfMvqVICC55PPFDK6xz5EcndOaWFt91Mhr91nexrMY';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
